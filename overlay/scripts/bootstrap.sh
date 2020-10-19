b35b36cba55fc2c0f06914fb5d58645047d49283
Deanosim <4834976+Deanosim@users.noreply.github.com>
2020-10-20 03:44:34 +1100

    Update bootstrap.sh

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -4,7 +4,7 @@ set -e
 # Preprocess UPSTREAM_DNS to allow for multiple resolvers using the same syntax as lancache-dns
 export UPSTREAM_DNS="$(echo -n "${UPSTREAM_DNS}" | sed 's/[;]/ /g')"
 
-sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/"    /etc/sniproxy.conf
+#sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/"    /etc/sniproxy.conf
 #sed -i "s/NETFLIX_USA/${NETFLIX_USA}/"    /etc/sniproxy.conf
 
 
